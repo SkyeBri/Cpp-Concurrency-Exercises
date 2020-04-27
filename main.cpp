429a1bcaff2d24cca614710d269526033da4895b
Lindsey Pittenger <lindsey.b.pittenger.ctr@mail.mil>
2020-04-27 14:46:16 -0500

    Solution 1 - Hello World

--- main.cpp
+++ main.cpp
@@ -1,25 +1,54 @@
 #include <iostream> // for cout, cin, ...
 #include <thread> // for threads
 #include <chrono> // for wibbly-wobbly time
+#include <unistd.h> // for sleep
+#include <vector>
+
 
 void helloWorld(std::string adj, int id)
 {
     std::cout << "Hello " << adj << " World " << id << "!" << std::endl;
+    sleep(2);
+}
+
+double helloSerialWorlds(int numTimes)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for(int i = 0; i < numTimes; ++i)
+    {
+        helloWorld("Serial", i);
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration<double>(stop-start).count();
+}
+
+double helloConcurrentWorlds(int numTimes)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::vector<std::thread> threads;
+    threads.resize(numTimes);
+    for(int i = 0; i < numTimes; ++i)
+    {
+        threads.at(i) = std::thread(helloWorld, "Concurrent", i);
+    }
+
+    for(auto &t : threads)
+    {
+        t.join();
+    }
+    auto stop = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration<double>(stop-start).count();
 }
 
 int main(int argc, char *argv[])
 {
-    // TODO:
-    // 1. Create a function called helloSerialWorld(int numTimes)
-    //    * Start a timer at the top of the function
-    //    * call helloWorld() numTimes with different ids, passing in "Serial" as the adj argument
-    //    * Stop the timer and print how long the function took
-    //
-    // 2. Create a function called helloConcurrentWorld(int numTimes)
-    //    * Start a timer at the top of the function
-    //    * call helloWorld() numTimes, passing in "Concurrent" as the adj argument; this time spawn a thread for each call
-    //    * join threads
-    //    * Stop timer and print how long the function took
-    //
-    // 3. Call both functions in main with the same numTimes to see if/where multithreading makes a difference for this task
+    int numTimes = 3;
+    double serial = helloSerialWorlds(numTimes);
+    double concur = helloConcurrentWorlds(numTimes);
+    std::cout << std::endl << "Num Times: " << numTimes << std::endl;
+    std::cout << "Serial Duration : " << serial <<std::endl;
+    std::cout << "Concurrent Duration: " << concur << std::endl;
 }
